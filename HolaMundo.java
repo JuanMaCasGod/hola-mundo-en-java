6125a2b5050f41b2d2e7b269e11f120248112ee9
Juan Manuel Castillo <juanma.desconocido@gmail.com>
2020-09-17 13:09:28 +0200

    Colores definidor

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,9 @@
 //mi primer comentario
 Public class HolaMundo{
 	public static void main(String[] args){
+		String naranja = "\033[33m";
+		String azul = "|o33[34m";
+		
 		System.out.println("HolaMundo");
 		System.out.println("Nuevo HolaMundo");
 	}
