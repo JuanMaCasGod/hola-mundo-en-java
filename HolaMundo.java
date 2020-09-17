f573e3766957c14f534c036c29391681126c4f2d
Juan Manuel Castillo <juanma.desconocido@gmail.com>
2020-09-17 13:11:00 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,7 +2,7 @@
 Public class HolaMundo{
 	public static void main(String[] args){
 		String naranja = "\033[33m";
-		String azul = "|o33[34m";
+		String azul = "\033[34m";
 		
 		System.out.println("HolaMundo");
 		System.out.println("Nuevo HolaMundo");
